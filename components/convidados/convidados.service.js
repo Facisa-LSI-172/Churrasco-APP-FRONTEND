1211ffe17f5dd3da1ad96ce84e28c00bd6837e12
Marcelo Brito <marcelobritowd@hotmail.com>
2017-11-17 19:23:01 -0300

    Service dos convidados refatorado

--- components/convidados/convidados.service.js
+++ components/convidados/convidados.service.js
@@ -1,19 +1,32 @@
 angular.module('meuChurrascoApp')
-.service('ConvidadosService', function ($http) {
+  .service('ConvidadosService', function ($http, appConstants) {
 
-  // const base_url = "http://192.168.34.118:8080/meuchurrascoapp/webapi/";
-  // const get_all_usuarios = "usuario/getall";
-  // const get_one_usuario = "usuario/get/";
+  this.getTodosUsuarios = function () {
+    $http.get(appConstants.URL + 'usuario/getall')
+      .then(function (response) {
+        return response.data;
 
-  // this.getTodosUsuarios = function () {
-  //   $http.get(base_url + get_all_usuarios)
-  //     .then(function (response) {
-  //       return response.data;
-  //     },
-  //     function errorCallback(response) {
-  //       console.log('Erro ao buscar todos os usuários!');
-  //     }
-  //   );
-  // }
+      }).catch(function (e) {
+        console.log('Error: ', e);
+        throw e;
 
+      }).finally(function () {
+        console.log('This finally block');
+      });
+  }
+
+  this.getUmUsuarios = function (id) {
+    $http.get(appConstants.URL + 'usuario/get/' + id)
+      .then(function (response) {
+        return response.data;
+
+      }).catch(function (e) {
+        console.log('Error: ', e);
+        throw e;
+
+      }).finally(function () {
+        console.log('This finally block');
+      });
+  }
+  
 });
